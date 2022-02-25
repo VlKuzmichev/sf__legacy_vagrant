39af68bd0db04ead29a91d4360e188624d61471b
VlKuzmichev <echo_vk1@yahoo.com>
2022-02-25 21:39:16 +0900

    first commit

---
+++ postgre.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo apt install curl ca-certificates gnupg
+
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+sudo apt-get update
+
+sudo apt install -y postgresql-8.4
+
